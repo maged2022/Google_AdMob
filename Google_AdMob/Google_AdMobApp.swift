f71a4e5c813c7a9981b4cf6ba8d2c16164e06255
maged <maged12345>
2025-05-02 19:07:54 +0300

    Initial Commit

---
+++ Google_AdMob/Google_AdMobApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_AdMobApp.swift
+//  Google_AdMob
+//
+//  Created by maged on 02/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Google_AdMobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
