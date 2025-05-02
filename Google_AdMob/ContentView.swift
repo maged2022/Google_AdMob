782cd3fcc722a8a775e40dafb3c4b78e70d4969b
maged <maged12345>
2025-05-02 19:20:15 +0300

    [ Feature ] Add Gooble Mobile Ads using Swift Package Manager

--- Google_AdMob/ContentView.swift
+++ Google_AdMob/ContentView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        HStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Text("Google AdMob!")
+            .font(.largeTitle)        }
         .padding()
     }
 }
